5522e37f4df1da22084e93f35f71afca4db02cc5
Zach DiMarco <zach.dimarco@pointsbet.com>
2023-09-25 16:47:06 -0600

    don't expand tabs

--- .config/nvim/lua/conf/settings.lua
+++ .config/nvim/lua/conf/settings.lua
@@ -5,7 +5,7 @@ vim.opt.wrap = false
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false 
 vim.opt.smartindent = true
 -- undo
 vim.opt.swapfile = false
