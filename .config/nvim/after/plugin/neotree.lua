a34a4e4768ca485f5b837eb68e6319a5814611fe
Zach DiMarco <zach.dimarco@pointsbet.com>
2023-11-14 12:22:35 -0500

    nvim: configure tree

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -1,6 +1,11 @@
 local tree = require('neo-tree')
 
-vim.keymap.set('n', '<leader>b', ':Neotree toggle show right<cr>') 
+tree.setup({
+	hide_dotfiles = false,
+	hide_gitignored = false,
+})
+
+vim.keymap.set('n', '<leader>b', ':Neotree toggle focus right<cr>')
 vim.keymap.set('n', '<leader>s', ':Neotree float git_status<cr>')
-vim.keymap.set('n', '|', ':Neotree reveal<cr>')
+vim.keymap.set('n', '|', ':Neotree reveal toggle<cr>')
 vim.keymap.set('n', '\\', ':Neotree toggle current reveal_force_cwd<cr>') 
