50c2808a7d8f5bf06110059a144b78abdb311b7c
zdim <zdim227@gmail.com>
2024-01-28 09:55:35 -0700

    nvim: resolve ts-context-commentstring warnings

--- .config/nvim/after/plugin/comment.lua
+++ .config/nvim/after/plugin/comment.lua
@@ -2,3 +2,7 @@ require('Comment').setup({
 	pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
 })
 
+require('ts_context_commentstring').setup {
+	enable_autocmd = false
+}
+
