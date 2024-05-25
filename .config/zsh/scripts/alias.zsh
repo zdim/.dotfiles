d3be1f67a4be2c37d239b0048cd5667420a85d72
Zach DiMarco <zach.dimarco@betfanatics.com>
2024-05-24 18:17:47 -0600

    zsh: add alias for lazygit & cors-friendly browser

--- .config/zsh/scripts/alias.zsh
+++ .config/zsh/scripts/alias.zsh
@@ -12,3 +12,7 @@ alias vim='nvim'
 
 # open all these files in nvim
 alias -s {js,jsx,html,ts,tsx,css,md}=nvim
+
+alias lg='lazygit'
+
+alias cors='open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security'
