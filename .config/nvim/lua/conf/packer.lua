dfed02e8aca6de60979bd530dfd82df34c3a7374
Zach DiMarco <zach.dimarco@betfanatics.com>
2024-05-24 18:17:14 -0600

    nvim: packer bootstrap

--- .config/nvim/lua/conf/packer.lua
+++ .config/nvim/lua/conf/packer.lua
@@ -1,4 +1,17 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
 vim.cmd [[packadd packer.nvim]]
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
@@ -58,6 +71,8 @@ return require('packer').startup(function(use)
 		}
 	}
 
+	use 'udalov/kotlin-vim'
+
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
@@ -83,4 +98,9 @@ return require('packer').startup(function(use)
 
 	use 'stevearc/dressing.nvim'
 	
+	-- bootstrap packer itself
+	-- must go at the end
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
