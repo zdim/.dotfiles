4df15f40c4f243c534732b1602829c7fb9e87456
Zach DiMarco <zach.dimarco@betfanatics.com>
2024-12-20 11:32:21 -0500

    nvim: actually fix ts_ls. also, add note about eslint 4.10.0

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,6 +5,7 @@ lsp.preset("recommended")
 
 lsp.ensure_installed({
 	'ts_ls',
+	-- NOTE: eslint 4.10.0 has an issue, prefer 4.8.0
 	'eslint',
 	'rust_analyzer',
 	'gopls',
@@ -15,7 +16,7 @@ lsp.ensure_installed({
 })
 
 require('lspconfig').sourcekit.setup({})
-require('lspconfig')['ts_ls'].setup({})
+require('lspconfig').ts_ls.setup({})
 
 lsp.set_server_config({
 	on_init = function(client)
@@ -48,7 +49,6 @@ lsp.format_on_save({
     },
     servers = {
         ['eslint'] = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
-        ['ts_ls'] = { 'typescript', 'typescriptreact' },
         ['rust_analyzer'] = { 'rust' },
 		['swift'] = { 'sourcekit' },
 		['terraformls'] = { 'terraform', 'tf' },
