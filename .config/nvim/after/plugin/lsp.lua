5a0c0ac3f78fc36588ee9a624c02f7c837b97f35
Zach DiMarco <zach.dimarco@betfanatics.com>
2025-06-03 20:18:46 -0600

    nvim: clean up lsp config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,12 +3,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(event)
 		local opts = { buffer = event.buf }
 
+		vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format() end, opts)
 		vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
 		vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
 		vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
 		vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-		vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-		vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
 		vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
 		vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
 		vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
@@ -30,11 +29,14 @@ require('mason-lspconfig').setup({
 		eslint = function()
 			require('lspconfig').eslint.setup({
 				settings = {
+					codeActionOnSave = {
+						enable = true
+					},
 					format = true
 				}
 			})
 		end,
-		lua_ls = function()
+		["lua_ls"] = function()
 			require('lspconfig').lua_ls.setup({
 				capabilities = lsp_capabilities,
 				settings = {
